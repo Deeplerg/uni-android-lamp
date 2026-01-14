70e2a56861051cb4d9377737a3209ecca9b095ea
Deeplerg <47917846+Deeplerg@users.noreply.github.com>
2026-01-14 06:26:56 +0500

    Fix endpoint

--- app/src/main/java/com/example/somelesson/di/NetworkModule.kt
+++ app/src/main/java/com/example/somelesson/di/NetworkModule.kt
@@ -13,7 +13,7 @@ object NetworkModule {
     @Provides
     fun provideLampService(): LampService =
         Retrofit.Builder()
-            .baseUrl("http://192.168.0.100:8000/")
+            .baseUrl("http://195.133.53.179:1337/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create()
